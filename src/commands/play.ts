60ad3dbc878a30aca72f241724ecd4fb396b4eb0
Alessandro Rubino <aleruby97@hotmail.it>
2022-07-24 07:34:50 +0000

    Add option to play command

--- src/commands/play.ts
+++ src/commands/play.ts
@@ -1,10 +1,20 @@
-import { SlashCommandBuilder } from "discord.js";
+import { SlashCommandBuilder, SlashCommandIntegerOption } from "discord.js";
 import { Command } from "../types/Command";
 
 const play: Command = {
-    data: new SlashCommandBuilder().setName("p").setDescription("Play a song"),
+    data: new SlashCommandBuilder()
+    .setName("p")
+    .setDescription("Play a song")
+    .addIntegerOption(
+        new SlashCommandIntegerOption()
+        .setName("n")
+        .setDescription("song number")
+        .setRequired(true)
+    )
+    .setDMPermission(false),
     execute: async (interaction) => {
-        interaction.reply("hello")
+        const n = interaction.options.getInteger("n");
+        interaction.reply(`Option was ${n}`);
     }
 };
 
